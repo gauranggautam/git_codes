69794f9e75e9e82aa531c5df8f71a547112d7e85
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 11:03:45 -0400

    Updated Changes

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_.txt',
 'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_.txt',
 'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_.txt',
@@ -7,5 +9,9 @@ sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_.txt',
 prefix = r"C:\Data/Emile/20250606/"
 
 for file in sweeps_filename:
-    m=readfile(f'{prefix}{file}*')
+    full_file = f'{prefix}{file}*' 
+    m=readfile(full_file)
+    plt.pcolor(m[0], m[1],m[2])
+    plt.colorbar()
+    plt.title()
     figure();pcolor(m[0], m[1],m[2]));colorbar();
