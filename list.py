3869ad49808a8df9479460fc6be2f327d0fa1e98
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 10:28:45 -0400

    Updated Changes

---
+++ list.py
@@ -0,0 +1,6 @@
+'TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_%T.txt'
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_%T.txt'
+'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_%T.txt'
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt'
+'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_%T.txt'
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt'
