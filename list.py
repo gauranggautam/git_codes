c33665d596693f5df622b112953e8b25aa3f23a3
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 11:02:45 -0400

    Updated Changes

--- list.py
+++ list.py
@@ -1,10 +1,11 @@
-sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_%T.txt',
-'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_%T.txt',
-'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_%T.txt',
-'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt',
-'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_%T.txt',
-'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt']
+sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_.txt',
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_.txt',
+'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_.txt',
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_.txt',
+'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_.txt',
+'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_.txt']
 prefix = r"C:\Data/Emile/20250606/"
 
 for file in sweeps_filename:
-    m=readfile(f'{prefix}+')
+    m=readfile(f'{prefix}{file}*')
+    figure();pcolor(m[0], m[1],m[2]));colorbar();
