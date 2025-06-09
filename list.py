fb620cdb02a7ccd412c96d32f9a0895ea6b9f0bd
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 11:01:45 -0400

    Updated Changes

--- list.py
+++ list.py
@@ -4,6 +4,7 @@ sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_%T.txt',
 'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt',
 'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_%T.txt',
 'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_%T.txt']
-prefix = r'C:\Data\Emile/20250606\'
+prefix = r"C:\Data/Emile/20250606/"
 
-m=readfile('')
+for file in sweeps_filename:
+    m=readfile(f'{prefix}+')
