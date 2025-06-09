2b34fbb27519819b48e3fce180cb014e398529a6
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 13:18:46 -0400

    Updated Changes

--- list.py
+++ list.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_20250607-164406',
-'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_20250607-213321',
-'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_20250607-215416',
-'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_20250607-221330',
-'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_20250607-223417']
-prefix = "C:\Data\Emile/20250606"
+
+
+
+#sweeps_filename = ['TG5_SweepVg2+Y_Vg1=-10_Vgg=0_Vb=0_',
+'TG5_SweepVg2+Y_Vg1=10_Vgg=0_Vb=0_',
+'TG5_SweepVg1+Y_Vg2=-10_Vgg=0_Vb=0_',
+'TG5_SweepVg1+Y_Vg2=10_Vgg=0_Vb=0_',
+'TG5_SweepVb+Y_Vg2=-2.5_Vg1=10_Vgg=0_',
+'TG5_SweepVb+Y_Vg2=-10_Vg1=10_Vgg=0_',
+'TG5_SweepVb+Y_Vg1=-2.5_Vg2=10_Vgg=0_',
+'TG5_SweepVb+Y_Vg1=-10_Vg2=10_Vgg=0_']
+#prefix = "C:\Data\Emile/20250606"
+
+#sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_20250607-164406',
+#'TG5_SweepX+Vgg_Vg1=-10_Vg2=10_Vb=0_20250607-213321',
+#'TG5_SweepX+Vgg_Vg1=10_Vg2=-10_Vb=0_20250607-215416',
+#'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_20250607-221330',
+#'TG5_SweepX+Vgg_Vg1=10_Vg2=10_Vb=0_20250607-223417']
+#prefix = "C:\Data\Emile/20250606"
 
 
 fig, axes = plt.subplots(3,2)
