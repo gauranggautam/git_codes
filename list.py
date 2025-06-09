51274215bbf0a32de7507b463682e4b92a7cc56c
gabelectro <g.laliberte@usherbrooke.ca>
2025-06-09 11:24:46 -0400

    Updated Changes

--- list.py
+++ list.py
@@ -9,13 +9,15 @@ sweeps_filename = ['TG5_SweepX+Vgg_Vg1=0_Vg2=0_Vb=0_20250607-164406',
 'TG5_SweepX+Vgg_Vg1=-10_Vg2=-10_Vb=0_20250607-221330']
 prefix = "C:\Data\Emile/20250606"
 
+plt.figure()
+fig, axes = plt.subplot(2,3)
 for file in sweeps_filename:
     full_file = f'{prefix}\{file}*' 
     print(full_file)
     m=readfile(full_file)
-    plt.figure()
-    plt.pcolor(m[0], m[1],m[2])
-    plt.colorbar()
-    plt.title(file)
-    plt.savefig(f'{full_file[:-1]}_.png')
+    
+    axes.pcolor(m[0], m[1],m[2])
+    axes.colorbar()
+    axes.title(file)
+    fig.savefig(f'{full_file[:-1]}_.png')
 
